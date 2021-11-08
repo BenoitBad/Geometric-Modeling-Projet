dd48002a5bed27f23a3bb1b0522a47673a459bc7
Haldarys <jp45230@gmail.com>
2021-11-08 17:50:50 +0100

    Correction intersection segment sphere

--- Assets/Scripts/GeoFunc.cs
+++ Assets/Scripts/GeoFunc.cs
@@ -35,16 +35,18 @@ public class GeoFunc
         return true;
     }
 
-    public static bool InterSegmentSphere(Segment seg, Sphere sph, out Vector3 interPt, out Vector3 interNormal)
+    public static bool InterSegmentSphere(Segment seg, Sphere sph, out Vector3 interPt1, out Vector3 interPt2, out Vector3 interNormal1, out Vector3 interNormal2)
     {
-        interPt = Vector3.zero;
-        interNormal = Vector3.zero;
+        interPt1 = Vector3.zero;
+        interPt2 = Vector3.zero;
+        interNormal1 = Vector3.zero;
+        interNormal2 = Vector3.zero;
         Vector3 AB = seg.pt2 - seg.pt1;
         Vector3 cA = seg.pt1 - sph.center;
-        float a, b, c, t, delta;
-        a = Vector3.Dot(AB, AB) * Vector3.Dot(AB, AB);
+        float a, b, c, t1, t2, delta;
+        a = Vector3.Dot(AB, AB);
         b = 2 * Vector3.Dot(cA, AB);
-        c = Vector3.Dot(cA, cA) * Vector3.Dot(cA, cA) - sph.radius * sph.radius;
+        c = Vector3.Dot(cA, cA) - sph.radius * sph.radius;
         delta = b * b - 4 * a * c;
         if (delta < 0)
         {
@@ -52,13 +54,15 @@ public class GeoFunc
         }
         else if(Mathf.Approximately(delta, 0))
         {
-            t = -b / (2 * a);
+            t1 = -b / (2 * a);
         }
         else
         {
-            t = (-b + Mathf.Sqrt(delta)) / (2 * a);
+            t1 = (-b + Mathf.Sqrt(delta)) / (2 * a);
+            t2 = (-b - Mathf.Sqrt(delta)) / (2 * a);
+            interPt2 = seg.pt1 + t2 * AB;
         }
-        interPt = seg.pt1 + t * AB;
+        interPt1 = seg.pt1 + t1 * AB;
         return true;
     }
 
--- Assets/Scripts/InterSegmentSphere.cs
+++ Assets/Scripts/InterSegmentSphere.cs
@@ -1,34 +1,54 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InterSegmentSphere : MonoBehaviour
 {
     [SerializeField] GameObject segmentObject;
     [SerializeField] GameObject sphereObject;
-    [SerializeField] GameObject intersection;
+    [SerializeField] GameObject intersection1;
+    [SerializeField] GameObject intersection2;
 
     // Update is called once per frame
     void Update()
     {
 
-        Vector3 intersectionPos = checkIntersection();
-        if (intersectionPos != Vector3.zero)
+        List<Vector3> intersectionPos = checkIntersection();
+        if (intersectionPos[0] != Vector3.zero)
         {
-            intersection.SetActive(true);
-            intersection.transform.position = intersectionPos;
-            Debug.Log(intersectionPos.x + " " + intersectionPos.y + " " + intersectionPos.z);
-        } else
+            intersection1.SetActive(true);
+            intersection1.transform.position = intersectionPos[0];
+            //Debug.Log("InterPt1: " + intersectionPos[0].x + " " + intersectionPos[0].y + " " + intersectionPos[0].z);
+        }
+        else
         {
-            intersection.SetActive(false);
+            intersection1.SetActive(false);
+        }
+
+        if (intersectionPos.Count == 2 && intersectionPos[1] != Vector3.zero)
+        {
+            intersection2.SetActive(true);
+            intersection2.transform.position = intersectionPos[1];
+            //Debug.Log("InterPt2: " + intersectionPos[1].x + " " + intersectionPos[1].y + " " + intersectionPos[1].z);
+        }
+        else
+        {
+            intersection2.SetActive(false);
         }
     }
 
-    Vector3 checkIntersection()
+    List<Vector3> checkIntersection()
     {
         Segment segment = segmentObject.GetComponent<SegmentObject>().seg;
         Sphere sphere = sphereObject.GetComponent<SphereObject>().s;
-        Vector3 interPt;
-        Vector3 interNormal;
-        GeoFunc.InterSegmentSphere(segment, sphere, out interPt, out interNormal);
-        return interPt;
+        List<Vector3> listInter = new List<Vector3>();
+        Vector3 interPt1, interPt2;
+        Vector3 interNormal1, interNormal2;
+        GeoFunc.InterSegmentSphere(segment, sphere, out interPt1, out interPt2, out interNormal1, out interNormal2);
+        listInter.Add(interPt1);
+        if(interPt2 != Vector3.zero)
+        {
+            listInter.Add(interPt2);
+        }
+        return listInter;
     }
 }
